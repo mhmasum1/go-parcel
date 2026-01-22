fbc2f4d77fc830298567955dcc55cc88a9a284ce
Mahmudul Hasan Masum <mhmasum1@gmail.com>
2026-01-22 16:19:59 +0600

    Firebase auth status live tracker

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { AuthContext } from './AuthContext'
 import { auth } from '../Firebase/Firebase.init'
-import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
 
 const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
 
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(false);
+
     const registerUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password)
     }
     const signInUser = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password)
     }
 
     const signInGoogle = () => {
+        setLoading(true);
         return signInWithPopup(auth, googleProvider)
     }
 
 
+    // observer
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
     const authInfo = {
+        user,
+        loading,
         registerUser,
         signInUser,
         signInGoogle,
